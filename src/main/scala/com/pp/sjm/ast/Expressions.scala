50d0d1f81adf5dc5f702c59d3895380e79d79e83
Pawel Ptaszynski <pptaszynski@gmail.com>
2011-01-17 10:42:43 +0100

    Created separate file for expressions nodes.

---
+++ src/main/scala/com/pp/sjm/ast/Expressions.scala
@@ -0,0 +1,14 @@
+package com.pp.sjm.ast
+/**
+ * @author Pawel
+ * @date 2011/01/17
+ */
+
+import scala.collection.mutable.{HashSet,LinkedList}
+import scala.util.parsing.input.{Positional}
+
+import com.pp.sjm.symbols.{TypeClass,Type}
+
+trait Expressions extends TypedNode {
+	
+}
